3e6bc3e290cc005f2f8027df21c42d3303421946
Mikayil Quenum <mquenum@users.noreply.github.com>
2023-10-04 15:09:50 +0200

    Books data as a Scriptable Object

---
+++ Assets/Scripts/BooksData.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct BookReview
+{
+    public string author;
+    public float note;
+    [TextArea]
+    public string review;
+}
+
+[CreateAssetMenu(fileName = "NewBookData", menuName = "Book Data")]
+public class BooksData : ScriptableObject
+{
+    [Serializable]
+    public struct BookData
+    {
+        public string title;
+        public string author;
+        public string type;
+        public float averageNote;
+        [TextArea]
+        public string summary;
+        public BookReview[] reviews;
+    }
+
+    public BookData[] books;
+}
